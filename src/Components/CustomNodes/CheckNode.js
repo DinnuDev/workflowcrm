bd89cf02e64f055d00ceb7d1753e4e2158f14dee
Dinesh Vutukuru <dinesh.vutukuru@newdayusa.com>
2024-07-29 07:05:47 -0400

    segmented

--- src/Components/CustomNodes/CheckNode.js
+++ src/Components/CustomNodes/CheckNode.js
@@ -5,7 +5,16 @@ import {
   PlusOutlined,
 } from "@ant-design/icons";
 import { Handle, Position } from "@xyflow/react";
-import { Button, Drawer, Form, Input, Popover, Select, Space } from "antd";
+import {
+  Button,
+  Drawer,
+  Form,
+  Input,
+  Popover,
+  Segmented,
+  Select,
+  Space,
+} from "antd";
 import React, { useState } from "react";
 
 const { Option } = Select;
@@ -14,29 +23,33 @@ const nodeStyle = {
   padding: "10px",
   borderRadius: "5px",
   boxShadow: "0 2px 5px rgba(0,0,0,0.2)",
-  backgroundColor: "#e6f7ff", // Light blue background color
+  backgroundColor: "#e6f7ff",
   border: "1px solid #ddd",
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
   width: "150px",
-  height: "150px", // Adjusted for square shape
-  transform: "rotate(45deg)", // Rotate for rhombus shape
+  height: "150px",
+  transform: "rotate(45deg)",
 };
 
 const contentStyle = {
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
-  transform: "rotate(-45deg)", // Counter-rotate content to make it horizontal
-  width: "100%", // Ensure full width usage
-  height: "100%", // Ensure full height usage
+  transform: "rotate(-45deg)",
+  width: "100%",
+  height: "100%",
   flexDirection: "column",
 };
 
 const CheckNode = ({ id, data }) => {
   const [drawerVisible, setDrawerVisible] = useState(false);
   const [conditions, setConditions] = useState(data.conditions || []);
+  const [validation, setValidation] = useState(
+    data.validation || { field: "" }
+  );
+  const [selectedSegment, setSelectedSegment] = useState("Conditions");
   const [isSaved, setIsSaved] = useState(false);
 
   const showDrawer = () => {
@@ -57,10 +70,26 @@ const CheckNode = ({ id, data }) => {
     setConditions(newConditions);
   };
 
+  const handleValidationChange = (value) => {
+    setValidation({ field: value });
+  };
+
   const handleSave = () => {
-    data.onEdit(id, { conditions, actions: data.actions || [] });
+    if (typeof data.onEdit === "function") {
+      if (selectedSegment === "Conditions") {
+        data.onEdit(id, { conditions });
+      } else {
+        data.onEdit(id, { validation });
+      }
+    } else {
+      console.error("data.onEdit is not a function");
+    }
     setIsSaved(true);
     closeDrawer();
+  };
+
+  const getAvailableValidationOptions = () => {
+    return ["is valid state", "is valid license", "is valid SSN number"];
   };
 
   const popoverContent = (
@@ -84,7 +113,7 @@ const CheckNode = ({ id, data }) => {
       style={{
         ...nodeStyle,
         boxShadow: isSaved
-          ? "0 5px 15px rgba(0, 255, 0, 0.25)" // Change box shadow color to green
+          ? "0 5px 15px rgba(0, 255, 0, 0.25)"
           : nodeStyle.boxShadow,
       }}
     >
@@ -160,56 +189,87 @@ const CheckNode = ({ id, data }) => {
           </div>
         }
       >
-        <Form.Item>
-          <Button type="dashed" onClick={addCondition} icon={<PlusOutlined />}>
-            Add Condition
-          </Button>
-        </Form.Item>
-        <Form layout="vertical">
-          {conditions.map((condition, index) => (
-            <Space
-              key={index}
-              style={{ display: "flex", marginBottom: 8 }}
-              align="baseline"
-            >
-              <Form.Item style={{ width: "100px" }}>
-                <Select
-                  placeholder="Field"
-                  value={condition.field}
-                  onChange={(value) =>
-                    handleConditionChange(index, "field", value)
-                  }
+        <Space align="baseline" direction="vertical">
+          <Segmented
+            options={["Conditions", "Validations"]}
+            value={selectedSegment}
+            onChange={(value) => setSelectedSegment(value)}
+          />
+          {selectedSegment === "Conditions" ? (
+            <>
+              <Form.Item>
+                <Button
+                  type="dashed"
+                  onClick={addCondition}
+                  icon={<PlusOutlined />}
                 >
-                  <Option value="FICO">FICO</Option>
-                  <Option value="LTV">LTV</Option>
-                  <Option value="DTI">DTI</Option>
-                </Select>
+                  Add Expression
+                </Button>
               </Form.Item>
+              <Form layout="vertical">
+                {conditions.map((condition, index) => (
+                  <Space
+                    key={index}
+                    style={{ display: "flex", marginBottom: 8 }}
+                    align="baseline"
+                  >
+                    <Form.Item style={{ width: "100px" }}>
+                      <Select
+                        placeholder="Field"
+                        value={condition.field}
+                        onChange={(value) =>
+                          handleConditionChange(index, "field", value)
+                        }
+                      >
+                        <Option value="FICO">FICO</Option>
+                        <Option value="LTV">LTV</Option>
+                        <Option value="DTI">DTI</Option>
+                      </Select>
+                    </Form.Item>
+                    <Form.Item style={{ width: "150px" }}>
+                      <Select
+                        placeholder="Operator"
+                        value={condition.operator}
+                        onChange={(value) =>
+                          handleConditionChange(index, "operator", value)
+                        }
+                      >
+                        <Option value=">">Greater than</Option>
+                        <Option value="<">Less than</Option>
+                        <Option value="=">Equal to</Option>
+                      </Select>
+                    </Form.Item>
+                    <Form.Item style={{ width: "70px" }}>
+                      <Input
+                        placeholder="Value"
+                        value={condition.value}
+                        onChange={(e) =>
+                          handleConditionChange(index, "value", e.target.value)
+                        }
+                      />
+                    </Form.Item>
+                  </Space>
+                ))}
+              </Form>
+            </>
+          ) : (
+            <Form layout="vertical">
               <Form.Item style={{ width: "150px" }}>
                 <Select
-                  placeholder="Operator"
-                  value={condition.operator}
-                  onChange={(value) =>
-                    handleConditionChange(index, "operator", value)
-                  }
+                  placeholder="Validation"
+                  value={validation.field}
+                  onChange={handleValidationChange}
                 >
-                  <Option value=">">Greater than</Option>
-                  <Option value="<">Less than</Option>
-                  <Option value="=">Equal to</Option>
+                  {getAvailableValidationOptions().map((option) => (
+                    <Option key={option} value={option}>
+                      {option}
+                    </Option>
+                  ))}
                 </Select>
               </Form.Item>
-              <Form.Item style={{ width: "70px" }}>
-                <Input
-                  placeholder="Value"
-                  value={condition.value}
-                  onChange={(e) =>
-                    handleConditionChange(index, "value", e.target.value)
-                  }
-                />
-              </Form.Item>
-            </Space>
-          ))}
-        </Form>
+            </Form>
+          )}
+        </Space>
       </Drawer>
     </div>
   );
